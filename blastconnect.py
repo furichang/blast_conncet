a1252a8d1b7858e16641518cf18023fdbc624256
furichang <umemuramasahiro@gmail.com>
2024-04-10 18:56:32 +0900

    Update selenium webdriver setup in blastconnect.py
    This commit modifies the selenium webdriver setup in the blastconnect.py file. Changes include using Chrome's Incognito mode, adding service to the webdriver setup, as well as updating the code to wait for some time before quitting the driver. Also, the login details were replaced with actual ones.

--- blastconnect.py
+++ blastconnect.py
@@ -1,23 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium import webdriver
+from selenium.webdriver.chrome import service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
-# Assume you're using Chrome; adjust as needed for Firefox, Safari, etc.
-driver = webdriver.Chrome()
+options = Options()
+options.add_experimental_option("detach", True)
+options.add_argument("--incognito")
+#options.add_argument("--headless")
+driver_path="chromedriver.exe"
+service = Service(driver_path)
 
-# Navigate to the login page
+driver = webdriver.Chrome(options=options,service=service)
 driver.get("https://baseball-academy-japan.blastconnect.com/login")
 
 # pause for page to load
 time.sleep(2)
 
 # Identify the username and password input fields and enter your login credentials
-username = driver.find_element_by_id("mat-input-0")
-password = driver.find_element_by_id("mat-input-1")
+email = driver.find_element(By.NAME,"email")
+password = driver.find_element(By.NAME,"password")
 
-# Replace 'your_username' and 'your_password' with your actual login details.
-username.send_keys("your_username")
-password.send_keys("your_password")
+# Replace 'your_email' and 'your_password' with your actual login details.
+email.send_keys("umemura@aoyama-h.ed.jp")
+password.send_keys("masa0214")
 
+login_button = driver.find_element(By.CSS_SELECTOR,".btn-primary")
 # Mimic pressing the Enter key to submit the login form
-password.send_keys(Keys.RETURN)
+# password.send_keys(Keys.RETURN)
+login_button.click()
+time.sleep(3)
+
+#driver.quit()
+
