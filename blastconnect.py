174a838dc02d222d7997ea87f791e35a0aaaab59
furichang <umemuramasahiro@gmail.com>
2024-04-10 18:56:32 +0900

    Add Selenium script and update project settings
    Added a Selenium script for automated login to a web page. Updated various project settings within the .idea directory and provided configuration for "InspectionProfiles". Also set up proper .gitignore for the .idea directory.

--- blastconnect.py
+++ blastconnect.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium import webdriver
+from selenium.webdriver.chrome import service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-# Assume you're using Chrome; adjust as needed for Firefox, Safari, etc.
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options=options)
+options.add_experimental_option("detach", True)
 
 # Navigate to the login page
 driver.get("https://baseball-academy-japan.blastconnect.com/login")
@@ -12,12 +17,14 @@ driver.get("https://baseball-academy-japan.blastconnect.com/login")
 time.sleep(2)
 
 # Identify the username and password input fields and enter your login credentials
-username = driver.find_element_by_id("mat-input-0")
-password = driver.find_element_by_id("mat-input-1")
+email = driver.find_element(By.NAME,"email")
+password = driver.find_element(By.NAME,"password")
 
-# Replace 'your_username' and 'your_password' with your actual login details.
-username.send_keys("your_username")
+# Replace 'your_email' and 'your_password' with your actual login details.
+email.send_keys("your_email")
 password.send_keys("your_password")
 
+login_button = driver.find_element(By.CSS_SELECTOR,".btn-primary")
 # Mimic pressing the Enter key to submit the login form
-password.send_keys(Keys.RETURN)
+# password.send_keys(Keys.RETURN)
+login_button.click()
